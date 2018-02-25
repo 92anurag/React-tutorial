8a195eda4c3425b1f94871fa50c6ece4e5e7c106
Anurag SInghal <anusing@adobe.com>
2018-02-25 16:56:06 +0530

    displaying usage in stateless functional component

--- 004-refs/001-refs-to-interact-with-ui-elements/src/Components/AddColorForm.js
+++ 004-refs/001-refs-to-interact-with-ui-elements/src/Components/AddColorForm.js
@@ -24,3 +24,27 @@ export default class AddColorForm extends Component {
     );
   }
 }
+
+
+
+// usage of ref in stateless functional component
+const AddColorForm1 = ({onNewColor = f=>f}) => {
+  let _title, _color;
+  const submit = e => {
+    e.preventDefault();
+    onNewColor(_title.value, _color.value);
+    _title.value = ''
+    _color.value = '#000000'
+    _title.focus()
+  }
+  return (
+    <form onSubmit={submit}>
+           <input ref={input => _title = input}
+                  type="text"
+                  placeholder="color title..." required/>
+           <input ref={input => _color = input}
+                  type="color" required/>
+           <button>ADD</button>
+       </form>
+  );
+};
